0e99cf92e66211ba92e60d69ce92648e53723c24
0x5 <kmalalamry8@protonmail.com>
2023-09-07 15:55:51 +0300

    final touches

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use macroquad::prelude::*;
 
 #[macroquad::main("sprite stack")]
 async fn main() {
+    // a single radian.
     let rad: f32 = 1.0_f32.to_radians();
+    // set of images that define the car. 
+    // storing them in a vec is not good practice, usualy you create a single 
+    //spritesheet with the images in it. i did it like this here to simplify the code
     let images = vec![
         load_texture("assets/img_0.png").await.unwrap(),
         load_texture("assets/img_1.png").await.unwrap(),
@@ -12,16 +16,24 @@ async fn main() {
         load_texture("assets/img_5.png").await.unwrap(),
         load_texture("assets/img_6.png").await.unwrap(),
     ];
-    let mut frame = 0.0;
+    // 
+    let mut rotation = 0.0;
+    let mut spread = 0;
     loop {
         clear_background(DARKGRAY);
-        frame += rad;
-        render_stack(&images,Vec2::new(250.0,250.0),frame,2);
+        let input_tuple = (bool_int(is_key_pressed(KeyCode::A)),bool_int(is_key_pressed(KeyCode::D)));
+        spread += input_tuple.0;
+        spread -= input_tuple.1;
+        rotation += rad;
+        render_stack(&images,Vec2::new(250.0,250.0),rotation,spread);
+        draw_text("press 'A' to increment the spread",0.0,30.0,48.0,RED);
+        draw_text("press 'D' to decrement the spread",0.0,80.0,48.0,RED);
+        draw_text(&format!("spread: {}",spread),0.0,140.0,48.0,RED);
         next_frame().await;
     }
 }
 
-
+// spread is how much distance is wanted in between the images
 fn render_stack(images:&Vec<Texture2D>,pos:Vec2,rotation:f32,spread:i32) {
     for (i,image) in images.into_iter().enumerate() {
         let pos_x = pos.x - image.width()/2.0;
@@ -41,4 +53,9 @@ fn render_stack(images:&Vec<Texture2D>,pos:Vec2,rotation:f32,spread:i32) {
         );
         
     }
+}
+
+
+fn bool_int(b:bool) -> i32 {
+    if b {1} else {0}
 }
